a9e0543e64afcc26186b7dc7151f07701aaa70b3
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2022-06-19 21:13:58 +0200

    Add calc

---
+++ calculator.py
@@ -0,0 +1,9 @@
+"""Calculator library containing basic math operations"""
+
+def add(first_term, second_term):
+    """Add Two terms"""
+    return first_term + second_term
+
+def substract(first_term, second_term):
+    """Substract Two Terms"""
+    return first_term - second_term
