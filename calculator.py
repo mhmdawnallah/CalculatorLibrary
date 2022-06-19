c6bf6f80aa2e3986ce53ba0875034b228b3a761b
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2022-06-19 21:45:03 +0200

    test multiplication for claculator

--- calculator.py
+++ calculator.py
@@ -9,3 +9,7 @@ def add(first_term, second_term):
 def substract(first_term, second_term):
     """Substract Two Terms"""
     return first_term - second_term
+
+def multiply(first_term, second_term):
+    """Multiply Two Terms"""
+    return first_term * second_term
--- test_calc.py
+++ test_calc.py
@@ -1,11 +1,13 @@
 import pytest
 
-from calculator import *
+import calculator 
 
 def test_add():
-    assert 3 == add(1,2)
+    assert 3 == calculator.add(1,2)
 
 
 def test_substract():
-    assert 2 == substract(3,1)
+    assert 2 == calculator.substract(3,1)
 
+def test_multiplication():
+    assert 100 == calculator.multiply(10,10)
